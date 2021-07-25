80e526ea8a43cc1c7543d8b2ec1e5a2119ee9512
tpdn333 <tpdn333@gmail.com>
2021-07-25 21:55:39 +0900

    first

---
+++ vue-todo/src/component/TodoInput.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+    <div inputBox shadow>
+      <input type="text" v-model="newTodoItem" placeholder="할 일 추가" v-on:keyup.enter="addTodo">
+      <span class="addContainer" v-on:click="addTodo">
+        <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+
+  methods: {
+    addTodo() {
+      if (this.newTodoItem != "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        this.$emit('addTodo', value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
